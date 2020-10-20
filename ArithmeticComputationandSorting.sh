0025f62f3795fbe4a7bd6005106a37ca1889e80c
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-20 14:00:09 +0530

    added_third_computation

--- ArithmeticComputationandSorting.sh
+++ ArithmeticComputationandSorting.sh
@@ -6,3 +6,5 @@ first_computation=$(( $a+$b*$c ))
 echo first_computation:"$first_computation"
 second_computation=$(( $a*$b+$c ))
 echo second_computation:"$second_computation"
+third_computation=$(( $c+$a/$b ))
+echo third_computation:"$third_computation"
