77db187c5534d471d2746c1d17d72f9ada4cf1dd
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-20 13:53:35 +0530

    added_second_computation

--- ArithmeticComputationandSorting.sh
+++ ArithmeticComputationandSorting.sh
@@ -4,3 +4,5 @@ read -p "enter a value: " b
 read -p "enter a value: " c
 first_computation=$(( $a+$b*$c ))
 echo first_computation:"$first_computation"
+second_computation=$(( $a*$b+$c ))
+echo second_computation:"$second_computation"
